61eb8d296e30a45e271f23d8f3d9ebc7f33af86a
dnyaneshwar411 <dnyaneshwarpk20@gmail.com>
2025-01-09 11:38:33 +0530

    feat: minor chagnes

--- server/src/index.js
+++ server/src/index.js
@@ -9,6 +9,7 @@ export const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.get("/", (_, res) => res.status(200).json({ success: true, message: "The server is up and running!" }));
 app.use('/api', langflowRoutes);
 
 app.listen(PORT, () => console.log("serving requests on port- ", PORT));
